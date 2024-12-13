fd37e2122725e6f3898b0df49dc67721823399d8
ozge <karabayozge@gmail.com>
2024-12-14 01:45:14 +0300

    send email

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,4 +1,5 @@
 const Portfolio = require("../models/Portfolio");
+const nodemailer = require('nodemailer');
 exports.getIndexPage = async (req, res) => {
     try {
         const portfolios = await Portfolio.find({}).sort('-dateCreated');
@@ -16,5 +17,45 @@ exports.getIndexPage = async (req, res) => {
     } catch (error) {
       console.error('Error:', error);
       res.status(500).json({ message: 'Server Error' });
+    }
+  };
+
+  exports.sendEmail = async (req, res) => {
+    const outputMessage = `
+      <h1>Mail Details </h1>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Email: ${req.body.phone}</li>
+      </ul>
+      <h1>Message</h1>
+      <p>${req.body.message}</p>
+    `;
+  
+    try {
+      let testAccount = await nodemailer.createTestAccount();
+  
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass,
+        },
+      });
+  
+      let info = await transporter.sendMail({
+        from: '"Freelancer Project Contact Form" <no-reply@freelancer.com>',
+        to: 'test@example.com',
+        subject: 'Freelancer Project Contact Form New Message ✔',
+        html: outputMessage,
+      });
+  
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      res.status(200).redirect('/');
+    } catch (error) {
+      res.status(200).redirect('/');
     }
   };
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getIndexPage} = require('../controllers/pageController');
 const { getAddPage } = require('../controllers/pageController');
+const { sendEmail } = require('../controllers/pageController');
 
 const router = express.Router();
 
 router.get('/', getIndexPage);
 router.get('/add', getAddPage);
+router.route('/contact').post(sendEmail);
 module.exports = router;
