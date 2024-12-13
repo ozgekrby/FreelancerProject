084aa3f18b7a1cafdd8c47fa66ec7c2d3cc39e84
ozge <karabayozge@gmail.com>
2024-12-14 00:54:29 +0300

    mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const mongoose=require("mongoose");
 //TEMPLATE ENGINE
 app.set("view engine","ejs")
 //Middlewares
@@ -11,6 +11,18 @@ app.get("/", (req, res) => {
         page_name:"index"
       })
 });
+// Connect DB
+const connectWithRetry = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/freelancer_database');
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    console.log("Retrying connection in 5 seconds...");
+    setTimeout(connectWithRetry, 5000); 
+  }
+};
+connectWithRetry();
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
